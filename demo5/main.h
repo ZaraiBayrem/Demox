03672b31e1064ed49ecc249f3904260d0eca7c47
ZaraiBayrem <47055493+ZaraiBayrem@users.noreply.github.com>
2022-02-09 17:51:24 +0100

    Update main.h

--- demo5/main.h
+++ demo5/main.h
@@ -2,20 +2,8 @@
 /**
   ******************************************************************************
   * @file    ThreadX/Tx_Thread_Creation/Inc/main.h
-  * @author  MCD Application Team
-  * @brief   This file contains all the functions prototypes for the main.c
-  *          file.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
+  * @author  Bayrem ZARAI
+  * @brief   This file contains all the functions prototypes for the main.c file.
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -68,5 +56,3 @@ void Error_Handler(void);
 #endif
 
 #endif /* __MAIN_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
